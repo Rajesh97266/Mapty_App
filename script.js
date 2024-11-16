1478504f8b32068b622015307b8d4fa47e132506
Rajesh97266 <rajesh97266@gmail.com>
2024-11-16 12:23:53 +0530

    geo-location-added

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      //   console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+      console.log(
+        `https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D`
+      );
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+}
